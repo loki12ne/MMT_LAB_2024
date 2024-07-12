32982485a9f0ee1428bb998a89dcc5944b004f59
schewpidcat <163629153+schewpidcat@users.noreply.github.com>
2024-07-12 16:09:56 +0700

    Update client.py

--- client.py
+++ client.py
@@ -4,69 +4,52 @@ import os
 HOST = "127.0.0.1"
 PORT = 65432
 
-# File size is expected to be converted to bytes
-def receiveFile(client_socket, file_name, file_size):
-    # Receive file size
-    # file_size = int(client_socket.recv(1024).decode('utf-8')) #size in megabytes
-    # file_size *= 1e6 #size in bytes
 
-    # Receive file data by chunk = 1024 bytes
-    received_size = 0
-    with open(file_name, "wb") as file:
-        while received_size < file_size:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            file.write(data)
-            received_size += 1024
-            # Print PROGESSING BAR
+# file_names và file_sizes là 2 mảng nhận được sau khi đọc
+# danh sách các file từ server
+def receiveFile(client_socket, file_names, file_sizes):
+    for i in file_names:
+        if not isValidFile(file_names, file_names[i]):
+            return
 
-# Check whether the file exists in the file list
-def isValidFile(files, file_name):
-    if file_name not in files:
-        return False, -1 #-1 là index, idx = -1 thì file ko tồn tại trong mảng
-    else:
-        return True, files.index(file_name) #trả về idx của file_name để cập nhật file_size lun
-        
-def checkFileExistence(files, file_name):
+        received_size = 0
+        with open(file_names[i], "wb") as file:
+            while received_size < file_sizes[i]:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                file.write(data)
+                received_size += 1024
+            # 
+    
+
+def isValidFile(file_names, file):
+    # Check whether the file exists in the file list
+    if file not in file_names:
+        return False
+
     # Check whether the file has been downloaded
     program_path = os.getcwd()
-    file_path = os.path.join(program_path, file_name)
-    count = files.count(file_name)
-
+    file_path = os.path.join(program_path, file)
     if os.path.exists(file_path):
-        file_name = renameFile(file_name, count)
-
-def renameFile(file_name, count):
-    if count == 0:
-        return file_name
+        return False
     
-    # Insert number if the file has already been downloaded
-    name, ext = os.path.splitext(file_name)
-    new_name = f"{name} ({count}){ext}"
-    return new_name
+    # File is valid to download
+    return True
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
     client_socket.connect(server_address)
 
-    files_list = "input.txt" #jj đó
-
     try:
         while True:
-            # READ FILE LIST -> return file_names[], file_sizes[] (?)
-            file_name = input('Enter a file name: ')
-            client_socket.send(bytes(file_name, "utf8"))
-            
-            flag, i = isValidFile(file_names, file_name) #ktra ten file client nhap vao co ton tai
-            if flag:
-                file_size = file_sizes[i]
-                file_name = checkFileExistence(file_names, file_name) # ktra lai dong nay
-                receiveFile(client_socket, file_name, file_size)
-            else:
-                pass
-        
+            # HÀM ĐỌC FILE -> return file_names[], file_sizes[]
+            # =====================================================
+            file_names = ["File1.zip", "File2.zip"] #ảo
+            file_sizes = [15, 25]                   #ảo
+            # =====================================================
+            receiveFile(client_socket, file_names, file_sizes)
     except KeyboardInterrupt:
         client_socket.close()
     finally:
