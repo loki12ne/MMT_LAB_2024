3adbfb0ed686135f6bebabf31deb0734727a5689
loki12ne <158790218+loki12ne@users.noreply.github.com>
2024-07-26 13:23:02 +0700

    Update client.py (phần 1)

--- client.py
+++ client.py
@@ -1,34 +1,62 @@
 import socket
 import os
-import base64
+import struct
+import sys
+import time
 
 HOST = "127.0.0.1"
 PORT = 65432
 _file = "input.txt"
-# _file      là *.txt tên các file cần tải
-# file_names là [] nhận được sau khi đọc
+download_path = "D:\HCMUS\Y1 - S3\MMT\PJ_File-Downloading\SOCKET\SOCKET 1\download"
+borderline = "---///---/ongquatronroido//--xichlendumtuidi-///=="
+borderline_b = borderline.encode('utf-8')
 
+def gotoXY(x, y):
+    print(f"\033[{y};{x}H", end='')
 
-def sendRequest    (client_socket, _file):
-    with open(_file, "r") as f:
-        while True:
-            file_datas = f.read()
-            if not file_datas:
-                break
-            data = base64.b64encode(file_datas)       # Encoding the data to Base64
-            client_socket.send(data) # Chưa hiểu rõ lắm, nếu có sửa thì bái lạo nhé
+def sendRequest(client_socket, file_names): 
+    for name in file_names:                       
+        client_socket.send(name.encode('utf-8'))    
+        client_socket.send('\n'.encode('utf-8'))  
+    client_socket.send(borderline_b)  
 
+def extractFileList(chunk):  
+    lines = chunk.strip().split('\n')
+    file_names = []
+    
+    for line in lines:
+        file_name = line.strip()
+        if file_name:
+            file_names.append(file_name)
+    return file_names
 
-# Nhận ds file từ sv để in ra screen
+def getFileList(file_names):
+    file_list = []
+    for file_name in file_names:
+        name, size = file_name.rsplit(' ', 1)
+        file_list.append({"name": name, "size": int(size)})
+    return file_list
+
 def receiveFileList(client_socket):
+    data = ""
     while True:
-        data = client_socket.recv(1024)
-        if not data: 
+        chunk = client_socket.recv(1024).decode('utf-8')
+        if not chunk: 
             break
-        print(f"DTL gửi file coi !")
+        if borderline in chunk:
+            data += chunk.split(borderline, 1)[0]
+            break
+        data += chunk
+    file_names = extractFileList(data)
+    
+    if file_names:
+        file_list = getFileList(file_names)
+    print(f"File list from server: {HOST}")
+    for i in file_names:
+        print(i)
+    return file_list
 
-
-def readInput(_file):
+def readInput():
     file_names = []
     try:
         with open(_file, 'r') as f:
@@ -40,46 +68,75 @@ def readInput(_file):
         print(f"File {_file} not found.")
     return file_names
 
+def getSize(file_list, file_name):
+    for file in file_list:
+        if file["name"] == file_name:
+            return file["size"]
 
-def receiveFile(client_socket, file_names):
-    for i in file_names: ##########################################################
-        if not isValidFile(file_names, file_names[i]):
-            continue
+def receiveFile(client_socket, file_names, file_list): 
+    data_return = []
+    for file_name in file_names:
+        if isValidFile(file_list, file_name): 
+            data_return.append(file_name)
 
-        received_size = 0
-        data = client_socket.recv(1024) # Original binary data
-        file_size = base64.b64decode(data) # Decoding the Base64 data back to original # Potential risk: Khi data truyền vào kp Base64
-        
-        with open(file_names[i], "wb") as f:
+    file_names = data_return
+    
+    if not file_names:
+        return False
+
+    sendRequest(client_socket, file_names)
+    count_inter = 1
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    gotoXY(0, 0)
+
+    for file_name in file_names:
+        received_size = 0        
+
+        file_size_bytes = client_socket.recv(4)
+        file_size = struct.unpack('!I', file_size_bytes)[0]
+        file_path = os.path.join(download_path, file_name)
+        with open(file_path, "wb") as f:
             while True:
-                data = client_socket.recv(1024) # how to make sure recv đủ 1024
+                if received_size > file_size - 10240:
+                    data = client_socket.recv(file_size - received_size)
+                else:
+                    data = client_socket.recv(10240)
+                received_size += len(data)
                 if not data:
                     break
-                if data == b"---///---///---///==":
-                    break
-                    
-                data = base64.b64decode(data) # Potential risk: Khi data truyền vào kp Base64
-                f.write(data) # Q: Đang  "wb" thì line trên decode v có s k
-                received_size += len(data)
+                f.write(data)
 
                 # Progressing bar
+                if received_size > file_size:
+                    received_size = file_size
                 progress = (received_size / file_size) * 100
-                print(f'Downloading {file_names[i]} .... {progress:.2f}%')
-        
-        # Receive EOF
-        # data = client_socket.recv(1024)
-        # buf = data.decode('utf-8')
+                gotoXY(0, count_inter)
+                print(f'Downloading {file_name} .... {progress:.2f}%        ', end='')
+                if (received_size == file_size):
+                    break
 
+        count_inter = count_inter + 1
+    gotoXY(0, count_inter)    
+    return True
 
-def isValidFile(file_names, file):
+def isSupported(file_list, file):
+    for i in file_list:
+        if file == i["name"]:
+            return True
+    return False
+
+def isValidFile(file_list, file):                      
     # Check whether the file exists in the file list
-    if file not in file_names:
+    if not isSupported(file_list, file):
+        print(f"{file} is not supported.")       
         return False
 
     # Check whether the file has been downloaded
-    program_path = os.getcwd()
+    program_path = download_path
     file_path = os.path.join(program_path, file)
     if os.path.exists(file_path):
+        print(f"{file} has already been downloaded.")  
         return False
     
     # File is valid to download
@@ -90,16 +147,34 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
     client_socket.connect(server_address)
+    first_time = 1
+    success = 0
 
     try:
+        file_list = receiveFileList(client_socket) 
         while True:
-            file_names = readInput(_file)
+            if success:
+                print("All requested files received.")
+            if first_time:
+                input("ENTER to send the requests to server")
+            else:
+                input("ENTER to send the requests to server again")
+
+            #tùy chỉnh clear cho cả Windows và MacOs/Linux
+            os.system('cls' if os.name == 'nt' else 'clear')
+            file_names = readInput()
+            first_time = 0
             if not file_names:
                 client_socket.close()
                 break
-            receiveFile(client_socket, file_names)
+            if receiveFile(client_socket, file_names, file_list):
+                success = 1
+              
     except KeyboardInterrupt:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Disconnecting from server...")
         client_socket.close()
+        print("Disconnected.")
     finally:
         client_socket.close()
 
